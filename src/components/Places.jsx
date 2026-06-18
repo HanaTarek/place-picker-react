f3e43811ea676e63a63791ab3c69907e52459a98
HanaTarek <105328136+HanaTarek@users.noreply.github.com>
2026-06-18 23:37:14 +0300

    adding images url

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -11,7 +11,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace , is
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
 
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                <img src={`${import.meta.env.VITE_API_URL}/${place.image.src}`} alt={place.image.alt} />
                 <h3>{place.title}</h3>
               </button>
             </li>
